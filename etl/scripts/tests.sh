ac09724701ab51ea6a4a99519515c03dee92801d
Guilherme Beltramini <gcbeltramini@gmail.com>
2020-07-19 19:45:21 -0300

    Test Markdown links

--- etl/scripts/tests.sh
+++ etl/scripts/tests.sh
@@ -17,3 +17,7 @@ echo "Done!"
 echo "Running 'flake8' (linter for Python files)..."
 flake8
 echo "Done!"
+
+echo "Running 'remark validate-links' (validates internal links in Markdown files)..."
+remark --use validate-links --quiet --frail .
+echo "Done!"
