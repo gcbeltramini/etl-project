a9c3662c9373d05a7d1c91ef9efb36c44b5f08ba
Guilherme Beltramini <gcbeltramini@gmail.com>
2020-07-19 16:46:43 -0300

    Add linter tests

--- etl/airflow_home/plugins/helpers/constants.py
+++ etl/airflow_home/plugins/helpers/constants.py
@@ -7,19 +7,18 @@ class CSV_Table(NamedTuple):
     file_name: str
     delimiter_char: Optional[str] = ','
 
+
 airflow_home = os.environ['AIRFLOW_HOME']
 data_path = os.path.join(airflow_home, 'resources', 'data')
 QUERIES_PATH = os.path.join(airflow_home, 'resources', 'sql')
+
 SCHEMA_NAME = 'etl'
-CSV_TABLES = {
-    'airport_codes': CSV_Table(table_name='airport_codes',
-                               file_name=os.path.join(data_path,
-                                                      'airport-codes.csv')),
-    'global_temperatures': CSV_Table(table_name='global_temperatures',
-                                     file_name=os.path.join(data_path,
-                                                            'GlobalLandTemperaturesByCity.csv')),
-    'us_cities': CSV_Table(table_name='us_cities',
-                           file_name=os.path.join(data_path,
-                                                  'us-cities-demographics.csv'),
-                           delimiter_char=';'),
-}
+tables = [
+    ('airport_codes', 'airport-codes.csv', {}),
+    ('global_temperatures', 'GlobalLandTemperaturesByCity.csv', {}),
+    ('us_cities', 'us-cities-demographics.csv', {'delimiter_char': ';'}),
+]
+CSV_TABLES = {tt[0]: CSV_Table(table_name=tt[0],
+                               file_name=os.path.join(data_path, tt[1]),
+                               **tt[2])
+              for tt in tables}
--- etl/scripts/tests.sh
+++ etl/scripts/tests.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 cur_dir=$(dirname "${BASH_SOURCE[0]}")
 
 echo "Running 'shellcheck'..."
 pushd "$cur_dir" 1>/dev/null || exit 1
 find . -type f -name '*.sh' -exec shellcheck -x '{}' \;
 popd 1>/dev/null || exit 1
+echo "Done!"
+
+echo "Running 'markdownlint-cli'..."
+markdownlint .
+echo "Done!"
+
+echo "Running 'flake8'..."
+flake8
 echo "Done!"
