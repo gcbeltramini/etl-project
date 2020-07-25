524bd273856d9b20b3ae39edd028478e0c1c7ad5
Guilherme Beltramini <gcbeltramini@gmail.com>
2020-07-25 18:23:22 -0300

    Rename function param

--- etl/airflow_home/plugins/helpers/sql_queries.py
+++ etl/airflow_home/plugins/helpers/sql_queries.py
@@ -1,6 +1,6 @@
 import os
 
 
-def read_sql(filename: str, **query_params) -> str:
-    with open(os.path.realpath(filename), 'r') as f:
+def read_sql(sql_file: str, **query_params) -> str:
+    with open(os.path.realpath(sql_file), 'r') as f:
         return f.read().format(**query_params)
