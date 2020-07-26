6bc16e4bca64be9ee653efc2e6a585a71faf82e1
Guilherme Beltramini <gcbeltramini@gmail.com>
2020-07-26 11:48:01 -0300

    Use AIRFLOW_HOME in jupyter notebook

--- etl/scripts/run_jupyter.sh
+++ etl/scripts/run_jupyter.sh
@@ -4,6 +4,13 @@ cur_dir=$(dirname "${BASH_SOURCE[0]}")
 # shellcheck source=./utils.sh
 source "${cur_dir}/utils.sh"
 
+if [[ -n "${AIRFLOW_HOME}" ]]; then
+  echo "Using AIRFLOW_HOME='${AIRFLOW_HOME}'"
+else
+  echo "Please define the environment variable 'AIRFLOW_HOME'"
+  exit 1
+fi
+
 enable_conda_in_script
 conda activate "$CONDA_ENV_NAME"
 jupyter notebook
